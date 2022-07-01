30371f705b768d7770432bfbde523022dff856c8
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2022-07-01 11:29:09 +0200

    first commit

---
+++ src/main/java/gestordetareasspringboot/GestorDeTareasSpringbootApplication.java
@@ -0,0 +1,13 @@
+package gestordetareasspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeTareasSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeTareasSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gestordetareasspringboot/GestorDeTareasSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package gestordetareasspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeTareasSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
