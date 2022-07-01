1c9bfc7680c26d138175f32b3dc3ba81ea2c0b7c
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2022-07-01 16:10:14 +0200

    Task + Repository

---
+++ src/main/java/gestordetareasspringboot/task/Task.java
@@ -0,0 +1,74 @@
+package gestordetareasspringboot.task;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Task {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String description;
+	private String datetime;
+	@ManyToMany
+	private Set<User> users;
+	
+	public Task() {	}
+	
+	public Task(String name, String descrip) {
+		this.name = name;
+		this.description = descrip;
+		this.datetime = LocalDateTime.now().toString();
+		this.users = new HashSet<User>();
+	}
+	
+	public Long getId() {
+		return this.id;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
+	public void setDescription(String descr) {
+		this.description = descr;
+	}
+	
+	public String getDateTime() {
+		return this.datetime.toString();
+	}
+	
+	public void addUser(User user) throws Exception {
+		if(this.users.contains(user)) {
+			throw new Exception("User already assigned to this task");
+		}
+		else {
+			this.users.add(user);
+		}
+	}
+	
+	public void deleteUser(User user) throws Exception {
+		if(!this.users.contains(user)) {
+			throw new Exception("User not assigned to this task");
+		}
+		else {
+			this.users.remove(user);
+		}
+	}
+	
+	
+}
