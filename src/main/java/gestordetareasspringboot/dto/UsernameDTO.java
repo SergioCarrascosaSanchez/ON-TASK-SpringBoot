19770128704ea3a4b6cf8f29b200fc486f565828
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2022-07-02 10:32:21 +0200

    IdDTO + UsernameDTO

---
+++ src/main/java/gestordetareasspringboot/dto/UsernameDTO.java
@@ -0,0 +1,19 @@
+package gestordetareasspringboot.dto;
+
+public class UsernameDTO {
+	private String username;
+	
+	public UsernameDTO() {}
+	
+	public UsernameDTO(String username) {
+		this.username = username;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
