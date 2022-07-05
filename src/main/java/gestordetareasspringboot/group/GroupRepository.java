e6ed43df767ee0ec2dc028d6a5328854864c030b
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2022-07-05 20:32:58 +0200

    add FindByName GroupRepository + change deleteUser method Group class

--- src/main/java/gestordetareasspringboot/group/GroupRepository.java
+++ src/main/java/gestordetareasspringboot/group/GroupRepository.java
@@ -1,7 +1,11 @@
 package gestordetareasspringboot.group;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GroupRepository extends JpaRepository<Group, Long>{
+import gestordetareasspringboot.user.User;
 
+public interface GroupRepository extends JpaRepository<Group, Long>{
+	public Optional<Group> findByName(String name);
 }
