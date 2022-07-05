56e19ac854a93b9112979ae296f1db83afef2943
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2022-07-05 20:28:20 +0200

    Adding constructor or attributes in SimpleUserDTO TaskInfoDTO ListOfTaskDTO

--- src/main/java/gestordetareasspringboot/dto/TaskInfoDTO.java
+++ src/main/java/gestordetareasspringboot/dto/TaskInfoDTO.java
@@ -3,12 +3,14 @@ package gestordetareasspringboot.dto;
 import gestordetareasspringboot.task.Task;
 
 public class TaskInfoDTO {
+	private Long id;
 	private String name;
 	private String description;
 	
 	public TaskInfoDTO() {}
 	
 	public TaskInfoDTO(Task task) {
+		this.setId(task.getId());
 		this.name = task.getName();
 		this.description = task.getDescription();
 	}
@@ -24,5 +26,13 @@ public class TaskInfoDTO {
 	}
 	public void setDescription(String descripcion) {
 		this.description = descripcion;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 }
